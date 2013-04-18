ba09c21264c055f0e228d33111291795728c0900
pravin <pravin@pravin-XPS-L501X.(none)>
2013-04-18 11:38:26 +0530

    fixes #343

--- sites/all/themes/sigmah_theme/templates/block-right.tpl.php
+++ sites/all/themes/sigmah_theme/templates/block-right.tpl.php
@@ -5,11 +5,15 @@
 		<div class="content">
 			<?php print $block->content; ?>
 		</div>
-		
-		<form action="forum.php" method="get">
-			<input type="recherche" type="text" onfocus="if(this.value=='Rechercher')this.value='';" onblur="if(this.value=='')this.value='Rechercher';" id="forumRec" value="Rechercher" />
-			<input type="submit"  id="forumButton" value="OK" name="recherche" />
-		</form>
+<form action="/sigmah-website/admin" accept-charset="UTF-8" method="post" target="_self">
+ <input type="text" maxlength="128" name="custom_search_blocks_form_1" id="edit-custom-search-blocks-form-1-1"  class="pravinpatil">
+<input type="hidden" name="delta" id="edit-delta" value="1">
+<input type="hidden" name="form_build_id" id="form-43893f3c46014c27af1d281d5ab8591d" value="form-43893f3c46014c27af1d281d5ab8591d">
+<input type="hidden" name="form_token" id="edit-custom-search-blocks-form-1-form-token" value="0688842ecd0f57a725d9e1206d5362cf">
+<input type="hidden" name="form_id" id="edit-custom-search-blocks-form-1" value="custom_search_blocks_form_1">
+<input type="hidden" name="default_text" id="edit-default-text" value="" class="default-text">
+<input type="submit" name="op" id="forumButton" value="Search" name="recherche" >
+</form>
 		<?php print $edit_links; ?>
 	</div> <!-- /block -->
 <?php }else{ ?>
